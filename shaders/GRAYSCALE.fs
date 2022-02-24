8a1d75ddcc17c621789d6ac93bb15cef75f6950a
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2022-02-24 22:38:05 +0100

    Delete shaders directory

--- shaders/GRAYSCALE.fs
+++
@@ -1,9 +0,0 @@
-uniform float intensity = 1;
-
-vec4 effect( vec4 color, Image tex, vec2 uvs, vec2 screen_coords )
-{
-    vec4 px = Texel(tex, uvs) * color;
-    float grayscale = (px.r + px.g + px.b) / 3;
-
-    return px + (vec4(grayscale,grayscale,grayscale,1) - px) * intensity;
-}
