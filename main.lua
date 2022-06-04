1764d2b8422532e8ae542491ec80d32113bac63e
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2022-06-04 12:31:52 +0200

    rich presence

--- main.lua
+++ main.lua
@@ -1,3 +1,31 @@
+local discordRPC = require("discordRPC")
+
+local appId = require("applicationId")
+
+function discordRPC.ready(userId, username, discriminator, avatar)
+    print(string.format("Discord: ready (%s, %s, %s, %s)", userId, username, discriminator, avatar))
+end
+
+function discordRPC.disconnected(errorCode, message)
+    print(string.format("Discord: disconnected (%d: %s)", errorCode, message))
+end
+
+function discordRPC.errored(errorCode, message)
+    print(string.format("Discord: error (%d: %s)", errorCode, message))
+end
+
+function discordRPC.joinGame(joinSecret)
+    print(string.format("Discord: join (%s)", joinSecret))
+end
+
+function discordRPC.spectateGame(spectateSecret)
+    print(string.format("Discord: spectate (%s)", spectateSecret))
+end
+
+function discordRPC.joinRequest(userId, username, discriminator, avatar)
+    print(string.format("Discord: join request (%s, %s, %s, %s)", userId, username, discriminator, avatar))
+    discordRPC.respond(userId, "yes")
+end
 
 -- All global values used in all scenes (display, textures, options, etc.)
 function love.load()
@@ -48,6 +76,25 @@ function love.load()
     scene = "menu"; firstScene = "menu"
     scenes[scene][2]()
 
+    -- Discord rpc
+
+    discordRPC.initialize(appId, true)
+    local now = os.time(os.date("*t"))
+    presence = {
+        state = "Action rougelike!",
+        details = "",
+        startTimestamp = nil,
+        endTimestamp = nil,
+        partyId = "",
+        partyMax = 2,
+        matchSecret = "",
+        joinSecret = "",
+        spectateSecret = "",
+        largeImageKey = "icon"
+    }
+
+    nextPresenceUpdate = 0
+
     -- Set joysticks
     JOYSTICKS = love.joystick.getJoysticks()
     JOYSTICK_LAST_PRESSES = {}
@@ -67,6 +114,16 @@ function love.load()
 end
 
 -- Play scenes
+function love.update()
+
+    if nextPresenceUpdate < love.timer.getTime() then
+        discordRPC.updatePresence(presence)
+        nextPresenceUpdate = love.timer.getTime() + 2.0
+    end
+    discordRPC.runCallbacks()
+
+end
+
 function love.draw()
 
     joystickJustPressedTriggerProcess(1)
@@ -192,4 +249,8 @@ function love.textinput(text)
 
     textInputed = text
 
+end
+
+function love.quit()
+    discordRPC.shutdown()
 end
